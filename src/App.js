7b8bc1fb9516c1104727491fd9c3265a70487f79
Shivam Kumar <life.shivam2394@gmail.com>
2024-06-23 04:02:40 +0530

    Adding the Folder configs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from './pages/Dashboard'
+import CourseList from './pages/CourseList'
+import CourseDetails from './pages/CourseDetails'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/" element={<CourseList />} />
+          <Route path="course-details" element={<CourseDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
